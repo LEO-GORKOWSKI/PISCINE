c2238ed616a292122511665bfb265b61e223346c
LEO-GORKOWSKI <leo.gorkowski@outlook.fr>
2024-11-25 11:31:15 +0100

    Commande Nageur

--- piscine-debut.py
+++ piscine-debut.py
@@ -1,29 +1,58 @@
 liste = [("Pierre","Dos",10),("Paul","Brasse",13),("Léa","Crawl",6), ("Léa","Brasse",8) ]
 commande = ''
 
-isAlive = True
-while isAlive:
-    commande = input("Que faut-il faire ? ")
+def cmd_exit():
+    tmp = input("En êtes vous sûr ? (o)ui/(n)on")
+    if tmp == 'o':
+         return False
+    else:
+         return True
 
-    if commande == 'ajout':
+def cmd_ajout(liste):
+        """Ajoute un évènement à la liste"""
         a = input("Qui nage ? ")
         b = input("quelle nage ? ")
         c = input("combien de longueur ? ")
         liste.append((a,b,c))
-        continue
-   
-    if commande == 'liste':
-        for elt in liste:
-            print(f"Prénom {elt[0]}, nage {elt[1]}, longueur {elt[2]}")
+
+def cmd_liste(liste):
+    """Affiche toutes les performances des nageurs"""
+    print("Prénom    -  nage  -  longeur")
+    print("-----------------------------")
+    for elt in liste:
+        print(f"{elt[0]:7} / {elt[1]} / {elt[2]}")
+
+def cmd_nageur(liste):
+     '''Affiche les performances d'un seul nageur'''
+     tmp = input("Quel nageur ?")
+     print("Performance de ", tmp)
+     print("nage  -  longeur")
+     print("----------------")
+     for elt in liste:
+        if elt[0]== tmp: 
+            print(f"{elt[1]:8} / {elt[2]}")
+     
+
+isAlive = True
+while isAlive:
+    commande = input("Que faut-il faire ? ")
+    if commande == 'ajout':
+        cmd_ajout(liste)
         continue
 
-    if commande == "exit":
-        tmp = input("En êtes vous sûr ? (o)ui/(n)on")
-        if tmp == 'o':
-            isAlive = False
+
+    if commande == 'liste':
+        cmd_liste(liste)
         continue
+
+
+    if commande == 'exit':
+        isAlive = cmd_exit()
+        continue
+ 
+    if commande == 'nageur':
+         cmd_nageur()
+         continue
 
     print(f"Commande {commande} inconnue")
-
-print ('test')
-
+    
