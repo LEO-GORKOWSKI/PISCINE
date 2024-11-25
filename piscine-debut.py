50cb14f8524bfce461192b494ff4a7a201f8a252
LEO-GORKOWSKI <leo.gorkowski@outlook.fr>
2024-11-25 10:02:07 +0100

    actualisation

--- piscine-debut.py
+++ piscine-debut.py
@@ -1,6 +1,7 @@
 liste = [("Pierre","Dos",10),("Paul","Brasse",13),("Léa","Crawl",6), ("Léa","Brasse",8) ]
 commande = ''
 
+isAlive = True
 while commande != 'exit':
     commande = input("Que faut-il faire ? ")
 
@@ -9,7 +10,16 @@ while commande != 'exit':
         b = input("quelle nage ? ")
         c = input("combien de longueur ? ")
         liste.append((a,b,c))
+        continue
    
     if commande == 'liste':
         for elt in liste:
             print(f"Prénom {elt[0]}, nage {elt[1]}, longueur {elt[2]}")
+        continue
+
+    if commande == "exit":
+        isAlive = False
+        continue
+
+    print(f"Commande {commande} inconnue")
+
